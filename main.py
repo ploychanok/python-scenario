41f34e9abafa2ac15ec9bde93b0d06e0ac5ba8a7
ploychanok <ploychanok.dm@gmail.com>
2023-12-19 15:57:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,17 @@ filtered_scenarios = filtered_scenarios_module[filtered_scenarios_module['Scenar
 
 # Display Chapter Titles for the selected Scenario Instances
 st.title(f"Chapter Titles for {selected_module} - {selected_scenario}")
-st.write(filtered_scenarios[['Chapter Title']])
+table_data = filtered_scenarios[['Chapter Title']]
 
+# Display the table of Chapter Titles
+st.table(table_data)
+
+# Allow the user to select a Chapter Title from the displayed table
+selected_chapter_title = st.text_input("Enter a Chapter Title:")
+if selected_chapter_title:
+    # Filter the data based on the selected Chapter Title
+    filtered_chapter = filtered_scenarios[filtered_scenarios['Chapter Title'] == selected_chapter_title]
+
+    # Display Chapter Summary directly without an expander
+    st.title(f"Chapter Summary for {selected_module} - {selected_scenario} - {selected_chapter_title}")
+    st.write(filtered_chapter['Chapter Summary'].iloc[0])  # Assuming there is only one unique Chapter Summary for the selected Chapter Title
