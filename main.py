70bec6be3d14b7aee13d3bd400d50287cdc01c45
ploychanok <ploychanok.dm@gmail.com>
2023-12-19 16:43:40 +0900

    update chapter summary

--- main.py
+++ main.py
@@ -21,14 +21,21 @@ filtered_scenarios = filtered_scenarios_module[filtered_scenarios_module['Scenar
 st.title(f"Chapter Titles for {selected_module} - {selected_scenario}")
 table_data = filtered_scenarios[['Chapter Title']]
 
-# Display the radio button list of Chapter Titles
-selected_chapter_title = st.radio("Select a Chapter Title:", table_data['Chapter Title'].unique())
+# Create two columns
+col1, col2 = st.columns(2)
 
-# Display Chapter Summary directly based on the selected Chapter Title
-if selected_chapter_title:
-    # Filter the data based on the selected Chapter Title
-    filtered_chapter = filtered_scenarios[filtered_scenarios['Chapter Title'] == selected_chapter_title]
+# Display the radio button list of Chapter Titles in the first column
+with col1:
+    selected_chapter_title = st.radio("Select a Chapter Title:", table_data['Chapter Title'].unique())
 
-    # Display Chapter Summary
-    st.title(f"Chapter Summary for {selected_module} - {selected_scenario} - {selected_chapter_title}")
-    st.write(filtered_chapter['Chapter Summary'].iloc[0])  # Assuming there is only one unique Chapter Summary for the selected Chapter Title
+# Display Chapter Summary, Book Name, and Chapter Number in the second column
+with col2:
+    if selected_chapter_title:
+        # Filter the data based on the selected Chapter Title
+        filtered_chapter = filtered_scenarios[filtered_scenarios['Chapter Title'] == selected_chapter_title]
+
+        # Display Chapter Summary, Book Name, and Chapter Number
+        st.title(f"Chapter Summary for {selected_module} - {selected_scenario} - {selected_chapter_title}")
+        st.write(f"Book Name: {filtered_chapter['Textbook'].iloc[0]}")
+        st.write(f"Chapter Number: {filtered_chapter['Chapter Number'].iloc[0]}")
+        st.write(filtered_chapter['Chapter Summary'].iloc[0])  # Assuming there is only one unique Chapter Summary for the selected Chapter Title
