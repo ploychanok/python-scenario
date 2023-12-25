d0462c27ef3b5c2d90aedd9776c8138f8434b68f
ploychanok <ploychanok.dm@gmail.com>
2023-12-25 19:49:02 +0900

    add search by library

--- main.py
+++ main.py
@@ -11,22 +11,46 @@ module_counts = learning_scenarios['Target Audience'].value_counts()
 
 # Sidebar
 st.sidebar.title("Scenario Analysis")
-selected_module = st.sidebar.selectbox("Select a Target Audience:", module_counts.index)
-selected_scenario = st.sidebar.selectbox(f"Select Scenario Instances for {selected_module}:", learning_scenarios['Scenario Instances'].unique())
-selected_chapter_title = st.sidebar.selectbox("Select a Chapter Title:", learning_scenarios[learning_scenarios['Scenario Instances'] == selected_scenario]['Chapter Title'].unique())
+option = st.sidebar.radio("Select Analysis Option:", ["By Module", "By Library"])
 
-# Filter data based on selected module, scenario, and chapter title
-filtered_chapter = learning_scenarios[
-    (learning_scenarios['Target Audience'] == selected_module) &
-    (learning_scenarios['Scenario Instances'] == selected_scenario) &
-    (learning_scenarios['Chapter Title'] == selected_chapter_title)
-]
+if option == "By Module":
+    selected_module = st.sidebar.selectbox("Select a Target Audience:", module_counts.index)
+    selected_scenario = st.sidebar.selectbox(f"Select Scenario Instances for {selected_module}:", learning_scenarios['Scenario Instances'].unique())
+    selected_chapter_title = st.sidebar.selectbox("Select a Chapter Title:", learning_scenarios[learning_scenarios['Scenario Instances'] == selected_scenario]['Chapter Title'].unique())
+
+    # Filter data based on selected module, scenario, and chapter title
+    filtered_chapter = learning_scenarios[
+        (learning_scenarios['Target Audience'] == selected_module) &
+        (learning_scenarios['Scenario Instances'] == selected_scenario) &
+        (learning_scenarios['Chapter Title'] == selected_chapter_title)
+    ]
+    
+elif option == "By Library":
+    # Extract unique libraries from the "Libraries" column and split them by ','
+    libraries = set(df['Libraries'].str.split(',').explode().str.strip())
+
+    selected_library = st.sidebar.selectbox("Select a Library:", list(libraries))
+
+    # Filter data based on the selected library
+    library_names = df['Libraries'].str.split(',').explode().str.strip()
+    chapters_for_library = learning_scenarios.loc[library_names == selected_library, 'Chapter Title'].unique()
+
+    st.sidebar.subheader(f"Chapter Titles mentioning {selected_library}")
+    selected_chapter_title = st.sidebar.selectbox("Select a Chapter Title:", chapters_for_library)
+
+    # Filter data based on the selected library and chapter title
+    filtered_chapter = learning_scenarios[
+        (library_names == selected_library) &
+        (learning_scenarios['Chapter Title'] == selected_chapter_title)
+    ]
 
 # Main content
-st.markdown(f"**{selected_module}** > **{selected_scenario}** > **{selected_chapter_title}**")
-
-# Display Chapter Summary, Book Name, Chapter Number, Code snippet, Code snippet description, and Download PDF link
 if not filtered_chapter.empty:
+    if option == "By Module":
+        st.markdown(f"**{selected_module}** > **{selected_scenario}** > **{selected_chapter_title}**")
+    elif option == "By Library":
+        st.markdown(f"**{selected_library}** > **{selected_chapter_title}**")
+
     st.markdown("### Chapter Summary")
     st.write(f"**Book Name:** {filtered_chapter['Dispaly Book Name'].iloc[0]}")
     st.write(f"**Chapter Number:** {filtered_chapter['Chapter Number'].iloc[0]}")
